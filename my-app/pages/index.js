0b7b7aad4ac653f0072c0c8da4c6e469f6848e8f
UB1302 <uttkarshbansal13@gmail.com>
2021-08-27 14:56:29 +0530

    Initial commit

---
+++ my-app/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+          <title>Webdev Newz</title>
+          <meta name="Keywords" content = "Web development"/>
+      </Head>
+      <h1>Welcome to Next js</h1>
+    </div>
+      )
+}
